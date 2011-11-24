2791a2a979e6eb502c19d015f416a8353ad881ae
Tom Jakubowski <tom@crystae.net>
2011-11-23 20:24:17 -0800

    rule defintion form fills out rule table

--- ruleN.js
+++ ruleN.js
@@ -15,3 +15,18 @@ function numberFromRule(bits) {
     return memo + (1 << power) * lookup[state];
   }, 0);
 }
+
+$(document).ready(function() {
+  $('#rule-form').submit(function() {
+    var n = $('#rule-number').val();
+    var rule = ruleFromNumber(n);
+    if (rule == undefined) {
+      alert('rule numbers must not be less than 0 or greater than 255.');
+      return false;
+    }
+    $('#rule-definition td').each(function(i) {
+      this.innerHTML = (rule[i] ? '1' : '0');
+    });
+    return false;
+  });
+});
